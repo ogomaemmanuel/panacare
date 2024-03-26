b5a1aabdaaf14fd29eb01cdc9dd0177739c2826a
Emmanuel <ogoma.emmanuel@gmail.com>
2024-03-26 22:53:52 +0300

    Fixed bottom padding on patient registration flow

--- lib/registration/patient_sign_up_page.dart
+++ lib/registration/patient_sign_up_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart' show FaIcon, FontAwesomeIcons;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart'
+    show FaIcon, FontAwesomeIcons;
 import 'package:pinput/pinput.dart';
 
 class PatientSignUpPage extends StatefulWidget {
@@ -18,11 +19,11 @@ class _PatientSignUpPageState extends State<PatientSignUpPage> {
   var phoneNumber = "";
   var fullName = "";
   var email = "";
-  var verificationCode="";
-  var timeRemaining=3.0;
+  var verificationCode = "";
+  var timeRemaining = 3.0;
   var _progress = 0.0;
   var stage = 1;
-  var totalSteps=4;
+  var totalSteps = 4;
 
   @override
   void initState() {
@@ -77,7 +78,7 @@ class _PatientSignUpPageState extends State<PatientSignUpPage> {
           ],
         ),
         Padding(
-          padding: const EdgeInsets.only(top:24.0),
+          padding: const EdgeInsets.only(top: 24.0),
           child: TextFormField(
             keyboardType: TextInputType.phone,
             autofocus: true,
@@ -90,7 +91,9 @@ class _PatientSignUpPageState extends State<PatientSignUpPage> {
           padding: EdgeInsets.only(top: 24.0),
           child: Row(
             children: [
-              Expanded(child: Text("By continuing you confirm that you are authorised to use this phone number and agree to receive an SMS text "))
+              Expanded(
+                  child: Text(
+                      "By continuing you confirm that you are authorised to use this phone number and agree to receive an SMS text "))
             ],
           ),
         )
@@ -113,7 +116,7 @@ class _PatientSignUpPageState extends State<PatientSignUpPage> {
           ],
         ),
         Padding(
-          padding: const EdgeInsets.only(top:24.0),
+          padding: const EdgeInsets.only(top: 24.0),
           child: TextFormField(
             keyboardType: TextInputType.emailAddress,
             autofocus: true,
@@ -122,8 +125,6 @@ class _PatientSignUpPageState extends State<PatientSignUpPage> {
             onSaved: (val) => fullName = val!,
           ),
         ),
-
-
       ],
     );
   }
@@ -142,40 +143,41 @@ class _PatientSignUpPageState extends State<PatientSignUpPage> {
             ),
           ],
         ),
-
         const Padding(
           padding: EdgeInsets.only(top: 24.0),
           child: Row(
             children: [
-              Flexible(child: Text("Enter the four digit activation code sent to your email and mobile number"))
+              Flexible(
+                  child: Text(
+                      "Enter the four digit activation code sent to your email and mobile number"))
             ],
           ),
         ),
-
         Padding(
-          padding: const EdgeInsets.only(top:24.0),
+          padding: const EdgeInsets.only(top: 24.0),
           child: Pinput(
             autofocus: true,
             mainAxisAlignment: MainAxisAlignment.start,
             onCompleted: (pin) => verificationCode = pin,
           ),
         ),
-        
         Padding(
-          padding: const EdgeInsets.only(top:24),
+          padding: const EdgeInsets.only(top: 24),
           child: Row(
-            children: [
-              Text("Time remaining $timeRemaining")
-            ],
+            children: [Text("Time remaining $timeRemaining")],
           ),
         ),
         GestureDetector(
           child: Row(
-
-            children: [ Padding(
-              padding: const EdgeInsets.only(top:24.0),
-              child: const Text("Resend code?",style: TextStyle(decoration: TextDecoration.underline, color: Colors.black)),
-            )],
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 24.0),
+                child: const Text("Resend code?",
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.black)),
+              )
+            ],
           ),
         )
       ],
@@ -194,70 +196,69 @@ class _PatientSignUpPageState extends State<PatientSignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        maintainBottomViewPadding: true,
-
-        child: Center(
-            child: Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                            padding: const EdgeInsets.only(bottom: 24.0),
-                            child: IconButton(
-                              style: IconButton.styleFrom(
-                                  foregroundColor:
-                                      const HSLColor.fromAHSL(1.0, 0, 0, 0)
-                                          .toColor(),
-                                  backgroundColor: const HSLColor.fromAHSL(
-                                          1.0, 192.0, 0.82, 0.90)
-                                      .toColor()),
-                              onPressed: () => _goToBack(context),
-                              icon: const FaIcon(FontAwesomeIcons.xmark),
-                            ))
-                      ],
-                    ),
-                    LinearProgressIndicator(
-                        valueColor:
-                            const AlwaysStoppedAnimation<Color>(Colors.blue),
-                        value: _progress),
-                    Form(
-                        child: [
-                      _buildFullNameInput(),
-                      _buildPhoneNumberInput(),
-                      _buildEmailInput(),
-                      _buildVerificationCodeInput()
-                    ][stage - 1])
-                  ],
+        body: SafeArea(
+          bottom: false,
+          maintainBottomViewPadding: true,
+          child: Center(
+              child: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                              padding: const EdgeInsets.only(bottom: 24.0),
+                              child: IconButton(
+                                style: IconButton.styleFrom(
+                                    foregroundColor:
+                                        const HSLColor.fromAHSL(1.0, 0, 0, 0)
+                                            .toColor(),
+                                    backgroundColor: const HSLColor.fromAHSL(
+                                            1.0, 192.0, 0.82, 0.90)
+                                        .toColor()),
+                                onPressed: () => _goToBack(context),
+                                icon: const FaIcon(FontAwesomeIcons.xmark),
+                              ))
+                        ],
+                      ),
+                      LinearProgressIndicator(
+                          valueColor:
+                              const AlwaysStoppedAnimation<Color>(Colors.blue),
+                          value: _progress),
+                      Form(
+                          child: [
+                        _buildFullNameInput(),
+                        _buildPhoneNumberInput(),
+                        _buildEmailInput(),
+                        _buildVerificationCodeInput()
+                      ][stage - 1])
+                    ],
+                  ),
                 ),
               ),
-            ),
-
+            ],
+          )),
+        ),
+        bottomSheet: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(
+                child: Container(
+              height: 48,
+              child: TextButton(
+                  onPressed: () => _updateStage(),
+                  style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.zero))),
+                  child: const Text("Next")),
+            ))
           ],
-        )),
-
-      ),
-   bottomSheet:
-   Row(
-     crossAxisAlignment:CrossAxisAlignment.end,
-     children: [
-       Expanded(
-           child: TextButton(
-               onPressed: () => _updateStage(),
-               style: TextButton.styleFrom(
-                   backgroundColor: Colors.blue,
-                   shape: const RoundedRectangleBorder(
-                       borderRadius: BorderRadius.all(Radius.zero))),
-               child: const Text("Next")))
-     ],
-   )
-    );
+        ));
   }
 }
